99784ea0d03bc6f71d00bc03edbac99d90398d0b
Daniel Solano Gómez <daniel@solanogomez.org>
2011-03-21 11:15:39 -0500

    Start implementing JNI version.

---
+++ ndk/src/decafbot/jni/GameChooserActivity.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright © 2011 Sattvik Software & Technology Resources, Ltd. Co.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of Sattvik Software & Technology Resources, Ltd. Co. nor
+ *    the names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package decafbot.jni;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+
+/**
+ * Activity that allows the user to choose a game to play.
+ *
+ * @author Daniel Solano Gómez
+ */
+public class GameChooserActivity extends Activity {
+    /** Log tag for the class. */
+    private static final String TAG = "GameChooser";
+    /** Whether or not this is the first time the activity has been loaded. */
+    private boolean firstTime;
+    /** Utility for talking. */
+    private Talker talker;
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        firstTime = (savedInstanceState == null);
+        setContentView(R.layout.game_chooser);
+        Button chooseButton = (Button) findViewById(R.id.choose_button);
+        chooseButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+                handleSelection();
+            }
+        });
+    }
+
+    /**
+     * Called when the user clicks the ‘choose’ button.  Handles launching of
+     * the desired game.
+     */
+    private void handleSelection() {
+        RadioGroup games = (RadioGroup) findViewById(R.id.game_chooser_list);
+        switch (games.getCheckedRadioButtonId()) {
+            case R.id.game_guess_the_number:
+                launchGuessTheNumber();
+                break;
+            case R.id.game_global_thermonuclear_war:
+                confirmGlobalThermonuclearWar();
+                break;
+        }
+    }
+
+    /** Starts the game ‘Choose the number’. */
+    private void launchGuessTheNumber() {
+        Log.d(TAG, "Launching guess the number");
+        startActivity(new Intent(this, GuessTheNumberActivity.class));
+        finish();
+    }
+
+    /** Confirm that the user wants to play ‘Global thermonuclear war’. */
+    private void confirmGlobalThermonuclearWar() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.game_name_global_thermonuclear_war);
+        builder.setMessage(R.string.gtw_confirm_game);
+        builder.setNegativeButton(R.string.later,
+                                  new DialogInterface.OnClickListener() {
+                                      public void onClick(
+                                              final DialogInterface dialogInterface,
+                                              final int i) {
+                                          launchGlobalThermonuclearWar();
+                                      }
+                                  });
+        builder.setPositiveButton(android.R.string.yes,
+                                  new DialogInterface.OnClickListener() {
+                                      public void onClick(
+                                              final DialogInterface dialogInterface,
+                                              final int i) {
+                                          launchGuessTheNumber();
+                                      }
+                                  });
+        builder.show();
+        talker.say(getString(R.string.gtw_confirm_game));
+    }
+
+    /** Launches the game ‘Global thermonuclear war’ */
+    private void launchGlobalThermonuclearWar() {
+        Log.d(TAG, "Launching global thermonuclear war");
+        startActivity(new Intent(this, GlobalThermonuclearWarActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        talker = new Talker(this);
+        if (firstTime) {
+            talker.say(getString(R.string.greetings));
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        talker.shutdown();
+        talker = null;
+        super.onPause();
+    }
+}
---
+++ ndk/src/decafbot/jni/Talker.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright © 2011 Sattvik Software & Technology Resources, Ltd. Co.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of Sattvik Software & Technology Resources, Ltd. Co. nor
+ *    the names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package decafbot.jni;
+
+import android.content.Context;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Utility class to facilitate use of the TTS engine.
+ *
+ * @author Daniel Solano Gómez
+ */
+public final class Talker implements TextToSpeech.OnInitListener {
+    /** The TTS synthesiser. */
+    private final TextToSpeech tts;
+    /** Latch used to ensure TTS is initialised before saying anything. */
+    private final CountDownLatch ttsLatch;
+    /** Flag for whether or not the talker has been shutdown. */
+    private final AtomicBoolean isShutdown;
+    /** Handles speech events asynchronously. */
+    private final Handler speechHandler = new Handler();
+
+    public Talker(final Context context) {
+        tts = new TextToSpeech(context, this);
+        ttsLatch = new CountDownLatch(1);
+        isShutdown = new AtomicBoolean(false);
+    }
+
+    /**
+     * Says the speech.
+     *
+     * @param speech the speech to speak
+     */
+    public void say(final String speech) {
+        sayLater(speech, 0L);
+    }
+
+    /**
+     * Delays saying the speech in an effort to allow the UI thread to handle
+     * things, such as starting up the TTS engine.
+     *
+     * @param speech what to say
+     * @param delay  how long to way before speaking, in milliseconds
+     */
+    private void sayLater(final String speech, final long delay) {
+        speechHandler.postDelayed(new Runnable() {
+            public void run() {
+                try {
+                    if (!isShutdown.get()) {
+                        if (ttsLatch.await(10, TimeUnit.MILLISECONDS)) {
+                            tts.speak(speech, TextToSpeech.QUEUE_FLUSH, null);
+                        } else {
+                            sayLater(speech, 100L);
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    // restore interrupt
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }, delay);
+    }
+
+    /**
+     * If the TTS has initialised successfully, says what needs to be said.
+     *
+     * @param status whether or not the TTS initialised successfully
+     */
+    public void onInit(final int status) {
+        ttsLatch.countDown();
+    }
+
+    /** Shuts down the TTS engine. */
+    public void shutdown() {
+        if (isShutdown.compareAndSet(true, false)) {
+            tts.shutdown();
+        }
+    }
+}
